ff95a549fa850de218afc168fdd709779977ce85
diligejy <sjy049@gmail.com>
2020-04-15 12:09:07 +0900

    로그인만들기1

--- fcdjango/fc_community/fcuser/views.py
+++ fcdjango/fc_community/fcuser/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from .models import Fcuser
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+
+        if not (username and password):
+            res_data['error'] = '값을 입력해주세요.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password, fcuser.password):
+                # 비밀번호가 일치, 로그인 처리
+                pass
+            else:
+                res_data['error'] = '비밀번호를 틀렸습니다'
+
+        return render(request, 'login.html', res_data)
 
 def register(request):
     if request.method == 'GET':
