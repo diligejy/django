02e5880167600d3cd6ef30dd5b175d83fe40ea4b
diligejy <sjy049@gmail.com>
2020-04-15 10:43:46 +0900

    form 태그 수정

--- fcdjango/fc_community/fcuser/views.py
+++ fcdjango/fc_community/fcuser/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        return render(request, 'register.html')
