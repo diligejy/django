aa0942bb6edec62ed9a7bcb4a05cf5e46dc1d892
diligejy <sjy049@gmail.com>
2020-04-16 16:26:53 +0900

    link 연결까지 구현

--- fcdjango/fc_community/fcuser/views.py
+++ fcdjango/fc_community/fcuser/views.py
@@ -7,12 +7,8 @@ from django.contrib.auth.hashers import make_password, check_password
 # Create your views here.
 
 def home(request):
-    user_id = request.session.get('user')
 
-    if user_id:
-        fcuser = Fcuser.objects.get(pk=user_id)
-        return HttpResponse(fcuser.username)
-    return HttpResponse('Home!')
+    return render(request, 'home.html')
 
 def login(request):
     if request.method == 'POST':
