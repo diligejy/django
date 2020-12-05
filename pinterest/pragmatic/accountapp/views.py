45f9137440f0f5b1994eac6d379b5f6d07050d9d
diligejy <sjy049@gmail.com>
2020-12-06 04:09:51 +0900

    detailView

--- pinterest/pragmatic/accountapp/views.py
+++ pinterest/pragmatic/accountapp/views.py
@@ -1,4 +1,5 @@
 from django.http.response import HttpResponseRedirect
+from django.views.generic.detail import DetailView
 from accountapp.models import HelloWorld
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -32,3 +33,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
