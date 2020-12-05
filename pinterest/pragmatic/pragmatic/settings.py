96fc56613bb65062e102afece437251ba64daa4e
diligejy <sjy049@gmail.com>
2020-12-06 02:28:02 +0900

    login logout

--- pinterest/pragmatic/pragmatic/settings.py
+++ pinterest/pragmatic/pragmatic/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os, environ
+import os
+import environ
+
+from django.urls.base import reverse_lazy
 
 env = environ.Env(
     # set casting, default value
@@ -22,7 +25,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 # reading .env file
 environ.Env.read_env(
-    env_file = os.path.join(BASE_DIR, '.env')
+    env_file=os.path.join(BASE_DIR, '.env')
 )
 
 
@@ -129,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
