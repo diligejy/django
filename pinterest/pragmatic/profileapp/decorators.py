2baee9533af93632d248e4dd1feff0189195c1c1
diligejy <sjy049@gmail.com>
2020-12-10 12:46:33 +0900

    오타수정

--- pinterest/pragmatic/profileapp/decorators.py
+++ pinterest/pragmatic/profileapp/decorators.py
@@ -5,7 +5,7 @@ from .models import Profile
 def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
         profile = Profile.objects.get(pk=kwargs['pk'])
-        if not profile.user == request.profile:
+        if not profile.user == request.user:
             return HttpResponseForbidden()
         return func(request, *args, **kwargs)
     return decorated
